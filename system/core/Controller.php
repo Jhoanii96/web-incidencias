128497a107be7fdcd7db3a99d1c5e0453c31aa39
Azuvel <azuvel@gointegreat.com>
2024-12-29 18:17:38 -0500

    634379ae

--- system/core/Controller.php
+++ system/core/Controller.php
@@ -1,1 +1,11 @@
-<?php class Controller{public function view($view,$data=[]){require_once 'app/views/'.$view.'View.php';}public function AdminView($view,$data=[]){require_once 'app/views/'.$view.'View.php';}} ?>
+<?php class Controller
+{
+    public function view($view, $data = [])
+    {
+        require_once MAIN_PROJECT . '/app/views/' . $view . 'View.php';
+    }
+    public function AdminView($view, $data = [])
+    {
+        require_once MAIN_PROJECT . '/app/views/' . $view . 'View.php';
+    }
+}
